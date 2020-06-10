7008123c66be06c3fb2141cea8ee89e874667427
Jannis <j.m.hoch@uu.nl>
2020-06-10 16:37:31 +0200

    trying to work with different time dtype in netcdf files

--- conflict_model/env_vars_nc.py
+++ conflict_model/env_vars_nc.py
@@ -8,7 +8,7 @@ import os, sys
 
 def rasterstats_GDP_PPP(gdf_in, config, sim_year, out_dir, saving_plots=False, showing_plots=False):
 
-    print('calculating zonal statistics per aggregation unit')
+    print('calculating GDP PPP mean per aggregation unit')
     
     nc_fo = os.path.join(config.get('general', 'input_dir'), 
                          config.get('env_vars', 'GDP_PPP'))
@@ -18,6 +18,7 @@ def rasterstats_GDP_PPP(gdf_in, config, sim_year, out_dir, saving_plots=False, s
     nc_var = nc_ds['GDP_per_capita_PPP']
 
     years = nc_ds['time'].values
+    print(years)
     years = years[years>=config.getint('settings', 'y_start')]
     years = years[years<=config.getint('settings', 'y_end')]
 
@@ -25,9 +26,9 @@ def rasterstats_GDP_PPP(gdf_in, config, sim_year, out_dir, saving_plots=False, s
 
     gdf = gdf_in.copy()
 
-    gdf['zonal_stats_min_' + str(sim_year)] = np.nan
-    gdf['zonal_stats_max_' + str(sim_year)] = np.nan
-    gdf['zonal_stats_mean_' + str(sim_year)] = np.nan
+    # gdf['zonal_stats_min_' + str(sim_year)] = np.nan
+    # gdf['zonal_stats_max_' + str(sim_year)] = np.nan
+    gdf['GDP_PPP_mean_' + str(sim_year)] = np.nan
 
     nc_arr = nc_var.sel(time=sim_year)
     nc_arr_vals = nc_arr.values
@@ -37,46 +38,18 @@ def rasterstats_GDP_PPP(gdf_in, config, sim_year, out_dir, saving_plots=False, s
     for i in range(len(gdf)):
         prov = gdf.iloc[i]
         zonal_stats = rstats.zonal_stats(prov.geometry, nc_arr_vals, affine=affine, stats="mean min max")
-        gdf.loc[i, 'zonal_stats_min_' + str(sim_year)] = zonal_stats[0]['min']
-        gdf.loc[i, 'zonal_stats_max_' + str(sim_year)] = zonal_stats[0]['max']
-        gdf.loc[i, 'zonal_stats_mean_' + str(sim_year)] = zonal_stats[0]['mean']
+        # gdf.loc[i, 'zonal_stats_min_' + str(sim_year)] = zonal_stats[0]['min']
+        # gdf.loc[i, 'zonal_stats_max_' + str(sim_year)] = zonal_stats[0]['max']
+        gdf.loc[i, 'GDP_PPP_mean_' + str(sim_year)] = zonal_stats[0]['mean']
 
     print('...DONE' + os.linesep)
 
-    fig, axes = plt.subplots(1, 3 , figsize=(20, 10))
+    fig, axes = plt.subplots(1, 1 , figsize=(20, 10))
 
     fig.suptitle(str(int(sim_year)), y=0.78)
 
-    gdf.plot(ax=axes[0],
-                column='zonal_stats_min_' + str(sim_year),
-                vmin=2000,
-                vmax=15000,
-                legend=True,
-                legend_kwds={'label': "min GDP_PPP",
-                                'orientation': "vertical",
-                                'shrink': 0.5,
-                                'extend': 'both'})
-    gdf.boundary.plot(ax=axes[0],
-                                color='0.5',
-                                linestyle=':',
-                                label='water province borders')
-
-    gdf.plot(ax=axes[1],
-                column='zonal_stats_max_' + str(sim_year),
-                vmin=2000,
-                vmax=15000,
-                legend=True,
-                legend_kwds={'label': "max GDP_PPP",
-                                'orientation': "vertical",
-                                'shrink': 0.5,
-                                'extend': 'both'})
-    gdf.boundary.plot(ax=axes[1],
-                                color='0.5',
-                                linestyle=':',
-                                label='water province borders')
-
-    gdf.plot(ax=axes[2],
-                column='zonal_stats_mean_' + str(sim_year),
+    gdf.plot(ax=axes,
+                column='GDP_PPP_mean_' + str(sim_year),
                 vmin=2000,
                 vmax=15000,
                 legend=True,
@@ -84,14 +57,14 @@ def rasterstats_GDP_PPP(gdf_in, config, sim_year, out_dir, saving_plots=False, s
                                 'orientation': "vertical",
                                 'shrink': 0.5,
                                 'extend': 'both'})
-    gdf.boundary.plot(ax=axes[2],
-                                color='0.5',
-                                linestyle=':',
-                                label='water province borders')
+    gdf.boundary.plot(ax=axes,
+                      color='0.5',
+                      linestyle=':',
+                      label='water province borders')
 
     plt.tight_layout()
 
-    plt_name = 'GDP_PPP_zonal_stats_' + str(int(sim_year)) + '.png'
+    plt_name = 'GDP_PPP_mean_' + str(int(sim_year)) + '.png'
     plt_name = os.path.join(out_dir, plt_name)
 
     if saving_plots:
@@ -102,16 +75,16 @@ def rasterstats_GDP_PPP(gdf_in, config, sim_year, out_dir, saving_plots=False, s
 
     return gdf
 
-def rasterstats_totalEvap(gdf_in, config, sim_year, out_dir, saving_plots=False, showing_plots=False):
+def rasterstats_totalEvap(gdf_in, config, sim_year, out_dir):
 
-    print('calculating zonal statistics per aggregation unit')
+    print('calculating evaporation mean per aggregation unit')
     
     nc_fo = os.path.join(config.get('general', 'input_dir'), 
                          config.get('env_vars', 'evaporation'))
 
     nc_ds = xr.open_dataset(nc_fo)
 
-    nc_var = nc_ds['total_Evaporation']
+    nc_var = nc_ds['total_evaporation']
 
     years = nc_ds['time'].values
     years = years[years>=config.getint('settings', 'y_start')]
@@ -121,7 +94,7 @@ def rasterstats_totalEvap(gdf_in, config, sim_year, out_dir, saving_plots=False,
 
     gdf = gdf_in.copy()
 
-    gdf['zonal_stats_mean_' + str(sim_year)] = np.nan
+    gdf['evap_mean_' + str(sim_year)] = np.nan
 
     nc_arr = nc_var.sel(time=sim_year)
     nc_arr_vals = nc_arr.values
@@ -131,7 +104,7 @@ def rasterstats_totalEvap(gdf_in, config, sim_year, out_dir, saving_plots=False,
     for i in range(len(gdf)):
         prov = gdf.iloc[i]
         zonal_stats = rstats.zonal_stats(prov.geometry, nc_arr_vals, affine=affine, stats="mean")
-        gdf.loc[i, 'zonal_stats_mean_' + str(sim_year)] = zonal_stats[0]['mean']
+        gdf.loc[i, 'evap_mean_' + str(sim_year)] = zonal_stats[0]['mean']
 
     print('...DONE' + os.linesep)
 
