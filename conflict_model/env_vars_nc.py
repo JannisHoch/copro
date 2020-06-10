abc8060c0ee332af56be7ac85688c7ab4de893d4
Jannis <j.m.hoch@uu.nl>
2020-06-10 11:15:51 +0200

    double checked ipynb and first changes in code for using more nc-files for vars

--- conflict_model/env_vars_nc.py
+++ conflict_model/env_vars_nc.py
@@ -101,3 +101,38 @@ def rasterstats_GDP_PPP(gdf_in, config, sim_year, out_dir, saving_plots=False, s
         plt.close()
 
     return gdf
+
+def rasterstats_totalEvap(gdf_in, config, sim_year, out_dir, saving_plots=False, showing_plots=False):
+
+    print('calculating zonal statistics per aggregation unit')
+    
+    nc_fo = os.path.join(config.get('general', 'input_dir'), 
+                         config.get('env_vars', 'evaporation'))
+
+    nc_ds = xr.open_dataset(nc_fo)
+
+    nc_var = nc_ds['total_Evaporation']
+
+    years = nc_ds['time'].values
+    years = years[years>=config.getint('settings', 'y_start')]
+    years = years[years<=config.getint('settings', 'y_end')]
+
+    affine = rio.open(nc_fo).transform
+
+    gdf = gdf_in.copy()
+
+    gdf['zonal_stats_mean_' + str(sim_year)] = np.nan
+
+    nc_arr = nc_var.sel(time=sim_year)
+    nc_arr_vals = nc_arr.values
+    if nc_arr_vals.size == 0:
+        raise ValueError('the data was found for this year in the nc-file {}, check if all is correct'.format(nc_fo))
+
+    for i in range(len(gdf)):
+        prov = gdf.iloc[i]
+        zonal_stats = rstats.zonal_stats(prov.geometry, nc_arr_vals, affine=affine, stats="mean")
+        gdf.loc[i, 'zonal_stats_mean_' + str(sim_year)] = zonal_stats[0]['mean']
+
+    print('...DONE' + os.linesep)
+
+    return gdf
