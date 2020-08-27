97b8fce142f49bd9cca39ade3f6fe3dbe34e9ebe
Jannis <j.m.hoch@uu.nl>
2020-08-27 16:34:51 +0200

    all-data and leave-one-out runs now executable as functions

--- conflict_model/machine_learning.py
+++ conflict_model/machine_learning.py
@@ -1,7 +1,8 @@
 import os
 import pandas as pd
 import numpy as np
-from sklearn import svm, neighbors, ensemble, preprocessing, model_selection
+import matplotlib.pyplot as plt
+from sklearn import svm, neighbors, ensemble, preprocessing, model_selection, metrics
 
 def define_scaling(config):
     """[summary]
@@ -108,3 +109,58 @@ def split_scale_train_test_split(X, Y, config, scaler):
     X_test = X_test[: , 1:]
 
     return X_train, X_test, y_train, y_test, X_train_geom, X_test_geom
+
+def fit_predict(X_train, y_train, X_test, clf):
+    """[summary]
+
+    Args:
+        X_train ([type]): [description]
+        y_train ([type]): [description]
+        X_test ([type]): [description]
+        clf ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """    
+
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+
+    y_prob = clf.predict_proba(X_test)
+
+    return y_pred, y_prob
+
+def evaluate_prediction(y_test, y_pred, y_prob, X_test, clf, out_dir):
+    """[summary]
+
+    Args:
+        y_test ([type]): [description]
+        y_pred ([type]): [description]
+        y_prob ([type]): [description]
+    """    
+
+    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+    print("Precision:", metrics.precision_score(y_test, y_pred))
+    print("Recall:", metrics.recall_score(y_test, y_pred))
+    print('F1 score: {0:0.2f}'.format(metrics.f1_score(y_test, y_pred)))
+    print('Brier score:', metrics.brier_score_loss(y_test, y_prob[:, 1]))
+    print('')
+
+    print(metrics.classification_report(y_test, y_pred))
+    print('')
+
+    fig, ax = plt.subplots(1, 1, figsize=(20,10))
+    disp = metrics.plot_precision_recall_curve(clf, X_test, y_test, ax=ax)
+    plt.savefig(os.path.join(out_dir, 'precision_recall_curve.png'), dpi=300)
+    plt.close()
+    
+    fig, ax = plt.subplots(1, 1, figsize=(8, 7))
+    metrics.plot_confusion_matrix(clf, X_test, y_test, ax=ax)
+    plt.savefig(os.path.join(out_dir, 'confusion_matrix.png'), dpi=300)
+    plt.close()
+
+    fig, ax = plt.subplots(1, 1, figsize=(20,10))
+    metrics.plot_roc_curve(clf, X_test, y_test, ax=ax)
+    plt.savefig(os.path.join(out_dir, 'ROC_curve.png'), dpi=300)               
+    plt.close()
