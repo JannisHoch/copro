4d00582c1c01a3997fe33d48655545388940adc7
Jannis <j.m.hoch@uu.nl>
2020-09-02 14:05:03 +0200

    removing option for sensibility analysis

--- conflict_model/machine_learning.py
+++ conflict_model/machine_learning.py
@@ -18,27 +18,20 @@ def define_scaling(config):
         [type]: [description]
     """    
 
-    if config.getboolean('general', 'sensitivity_analysis'):
-        scalers = [preprocessing.MinMaxScaler(),
-                   preprocessing.StandardScaler(),
-                   preprocessing.RobustScaler(),
-                   preprocessing.QuantileTransformer(random_state=42)]
-    
-    elif not config.getboolean('general', 'sensitivity_analysis'):
-        if config.get('machine_learning', 'scaler') == 'MinMaxScaler':
-            scalers = [preprocessing.MinMaxScaler()]
-        elif config.get('machine_learning', 'scaler') == 'StandardScaler':
-            scalers = [preprocessing.StandardScaler()]
-        elif config.get('machine_learning', 'scaler') == 'RobustScaler':
-            scalers = [preprocessing.RobustScaler()]
-        elif config.get('machine_learning', 'scaler') == 'QuantileTransformer':
-            scalers = [preprocessing.QuantileTransformer(random_state=42)]
-        else:
-            raise ValueError('no supported scaling-algorithm selected - choose between MinMaxScaler, StandardScaler, RobustScaler or QuantileTransformer')
+    if config.get('machine_learning', 'scaler') == 'MinMaxScaler':
+        scaler = [preprocessing.MinMaxScaler()]
+    elif config.get('machine_learning', 'scaler') == 'StandardScaler':
+        scaler = [preprocessing.StandardScaler()]
+    elif config.get('machine_learning', 'scaler') == 'RobustScaler':
+        scaler = [preprocessing.RobustScaler()]
+    elif config.get('machine_learning', 'scaler') == 'QuantileTransformer':
+        scaler = [preprocessing.QuantileTransformer(random_state=42)]
+    else:
+        raise ValueError('no supported scaling-algorithm selected - choose between MinMaxScaler, StandardScaler, RobustScaler or QuantileTransformer')
 
     print('chosen scaling method is {}'.format(scalers[0]))
 
-    return scalers
+    return scaler[0]
 
 def define_model(config):
     """[summary]
@@ -53,26 +46,18 @@ def define_model(config):
         [type]: [description]
     """    
     
-    if config.getboolean('general', 'sensitivity_analysis'):
-        clfs = [svm.NuSVC(nu=0.1, kernel='rbf', class_weight={1: 100}, random_state=42, probability=True, degree=10, gamma=10),
-                neighbors.KNeighborsClassifier(n_neighbors=10, weights='distance'),
-                ensemble.RandomForestClassifier(n_estimators=1000, class_weight={1: 100}, random_state=42)]
+    if config.get('machine_learning', 'model') == 'NuSVC':
+        clf = [svm.NuSVC(nu=0.1, kernel='rbf', class_weight={1: 100}, random_state=42, probability=True, degree=10, gamma=10)]
+    elif config.get('machine_learning', 'model') == 'KNeighborsClassifier':
+        clf = [neighbors.KNeighborsClassifier(n_neighbors=10, weights='distance')]
+    elif config.get('machine_learning', 'model') == 'RFClassifier':
+        clf = [ensemble.RandomForestClassifier(n_estimators=1000, class_weight={1: 100}, random_state=42)]
+    else:
+        raise ValueError('no supported ML model selected - choose between NuSVC, KNeighborsClassifier or RFClassifier')
 
-        print('sensitivity analysis specified - all supported models chosen for run')
-    
-    elif not config.getboolean('general', 'sensitivity_analysis'):
-        if config.get('machine_learning', 'model') == 'NuSVC':
-            clfs = [svm.NuSVC(nu=0.1, kernel='rbf', class_weight={1: 100}, random_state=42, probability=True, degree=10, gamma=10)]
-        elif config.get('machine_learning', 'model') == 'KNeighborsClassifier':
-            clfs = [neighbors.KNeighborsClassifier(n_neighbors=10, weights='distance')]
-        elif config.get('machine_learning', 'model') == 'RFClassifier':
-            clfs = [ensemble.RandomForestClassifier(n_estimators=1000, class_weight={1: 100}, random_state=42)]
-        else:
-            raise ValueError('no supported ML model selected - choose between NuSVC, KNeighborsClassifier or RFClassifier')
+    print('chosen ML model is {}'.format(clfs[0]))
 
-        print('chosen ML model is {}'.format(clfs[0]))
-
-    return clfs
+    return clf[0]
 
 #TODO: it may make sense to have the entire XY process chain as object-oriented code
 
