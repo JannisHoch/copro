2821ac0986ecae18b9c000e45edfd7fb8a1e6027
Jannis <j.m.hoch@uu.nl>
2020-09-02 14:55:22 +0200

    renaming scripts

--- conflict_model/pipeline.py
+++ conflict_model/pipeline.py
@@ -1,6 +1,6 @@
 #TODO: if that remains the only function in this py-file, then better move the function in the click-script and execute there
 
-from conflict_model import pipelines, data, machine_learning
+from conflict_model import models, data, machine_learning
 import os, sys
 
 
@@ -25,13 +25,13 @@ def prepare_ML(config):
 def run(X, Y, config, scaler, clf, out_dir):
 
     if config.getint('general', 'model') == 1:
-        y_df, y_gdf, eval_dict = pipelines.all_data(X, Y, config, scaler, clf, out_dir)
+        y_df, y_gdf, eval_dict = models.all_data(X, Y, config, scaler, clf, out_dir)
     elif config.getint('general', 'model') == 2:
-        y_df, y_gdf, eval_dict = pipelines.leave_one_out(X, Y, config, scaler, clf, out_dir)
+        y_df, y_gdf, eval_dict = models.leave_one_out(X, Y, config, scaler, clf, out_dir)
     elif config.getint('general', 'model') == 3:
-        y_df, y_gdf, eval_dict = pipelines.single_variables(X, Y, config, scaler, clf, out_dir)
+        y_df, y_gdf, eval_dict = models.single_variables(X, Y, config, scaler, clf, out_dir)
     elif config.getint('general', 'model') == 4:
-        y_df, y_gdf, eval_dict = pipelines.dubbelsteen(X, Y, config, scaler, clf, out_dir)
+        y_df, y_gdf, eval_dict = models.dubbelsteen(X, Y, config, scaler, clf, out_dir)
     else:
         raise ValueError('the specified model type in the cfg-file is invalid - specify either 1, 2, 3 or 4.')
 
