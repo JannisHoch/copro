baa8f38bb0822f5af1d09b7120fb6594cf6a49e6
Jannis <j.m.hoch@uu.nl>
2020-09-02 11:56:45 +0200

    number of conlicts per polygon in test sample visualized

--- conflict_model/evaluation.py
+++ conflict_model/evaluation.py
@@ -41,19 +41,31 @@ def evaluate_prediction(y_test, y_pred, y_prob, X_test, clf, out_dir):
 
 def get_average_hit(df, global_df):
 
+    #- create a dataframe containing the number of occurence per ID
     ID_count = df.ID.value_counts().to_frame().rename(columns={'ID':'ID_count'})
+    #- add column containing the IDs
     ID_count['ID'] = ID_count.index.values
+    #- set index with index named ID now
     ID_count.set_index(ID_count.ID, inplace=True)
+    #- remove column ID
     ID_count = ID_count.drop('ID', axis=1)
 
+    #- per polygon ID, compute sum of overall correct predictions and rename column name
     hit_count = df.overall_hit.groupby(df.ID).sum().to_frame().rename(columns={'overall_hit':'total_hits'})
 
+    #- per polygon ID, compute sum of all conflict data points and add to dataframe
+    hit_count['nr_of_test_confl'] = df.y_test.groupby(df.ID).sum()
+
+    #- merge the two dataframes with ID as key
     df_temp = pd.merge(ID_count, hit_count, on='ID')
 
+    #- compute average correct prediction rate by dividing sum of correct predictions with number of all predicionts
     df_temp['average_hit'] = df_temp.total_hits / df_temp.ID_count
 
+    #- merge with global dataframe containing IDs and geometry, and keep only those polygons occuring in test sample
     df_hit = pd.merge(df_temp, global_df, on='ID', how='left')
 
+    #- convert to geodataframe
     gdf_hit = gpd.GeoDataFrame(df_hit, geometry=df_hit.geometry)
 
     return df_hit, gdf_hit
