08692b3330045fd8f7fa931d69cd99e73014c218
Jannis <j.m.hoch@uu.nl>
2020-09-03 14:06:25 +0200

    now plotting ROC curve per n run and also mean curve

--- conflict_model/evaluation.py
+++ conflict_model/evaluation.py
@@ -32,7 +32,7 @@ def fill_out_df(out_df, y_df):
 
     return out_df
 
-def evaluate_prediction(y_test, y_pred, y_prob, X_test, clf):
+def evaluate_prediction(y_test, y_pred, y_prob, X_test, clf, config):
     """[summary]
 
     Args:
@@ -41,17 +41,18 @@ def evaluate_prediction(y_test, y_pred, y_prob, X_test, clf):
         y_prob ([type]): [description]
     """    
 
-    print("Accuracy: {0:0.3f}".format(metrics.accuracy_score(y_test, y_pred)))
-    print("Precision: {0:0.3f}".format(metrics.precision_score(y_test, y_pred)))
-    print("Recall: {0:0.3f}".format(metrics.recall_score(y_test, y_pred)))
-    print('F1 score: {0:0.3f}'.format(metrics.f1_score(y_test, y_pred)))
-    print('Brier loss score: {0:0.3f}'.format(metrics.brier_score_loss(y_test, y_prob[:, 1])))
-    print('Cohen-Kappa score: {0:0.3f}'.format(metrics.cohen_kappa_score(y_test, y_pred)))
-    print('ROC AUC score {0:0.3f}'.format(metrics.roc_auc_score(y_test, y_pred)))
-    print('')
+    if config.getboolean('general', 'verbose'):
+        print("Accuracy: {0:0.3f}".format(metrics.accuracy_score(y_test, y_pred)))
+        print("Precision: {0:0.3f}".format(metrics.precision_score(y_test, y_pred)))
+        print("Recall: {0:0.3f}".format(metrics.recall_score(y_test, y_pred)))
+        print('F1 score: {0:0.3f}'.format(metrics.f1_score(y_test, y_pred)))
+        print('Brier loss score: {0:0.3f}'.format(metrics.brier_score_loss(y_test, y_prob[:, 1])))
+        print('Cohen-Kappa score: {0:0.3f}'.format(metrics.cohen_kappa_score(y_test, y_pred)))
+        print('ROC AUC score {0:0.3f}'.format(metrics.roc_auc_score(y_test, y_prob[:, 1])))
+        print('')
 
-    print(metrics.classification_report(y_test, y_pred))
-    print('')
+        print(metrics.classification_report(y_test, y_pred))
+        print('')
 
     eval_dict = {'Accuracy': metrics.accuracy_score(y_test, y_pred),
                  'Precision': metrics.precision_score(y_test, y_pred),
@@ -59,7 +60,7 @@ def evaluate_prediction(y_test, y_pred, y_prob, X_test, clf):
                  'F1 score': metrics.f1_score(y_test, y_pred),
                  'Cohen-Kappa score': metrics.cohen_kappa_score(y_test, y_pred),
                  'Brier loss score': metrics.brier_score_loss(y_test, y_prob[:, 1]),
-                 'ROC AUC score': metrics.roc_auc_score(y_test, y_pred),
+                 'ROC AUC score': metrics.roc_auc_score(y_test, y_prob[:, 1]),
                 }
 
     return eval_dict
@@ -113,16 +114,43 @@ def plot_precision_recall_curve(clf, X_test, y_test, **kwargs):
 
     return disp
 
+def init_out_ROC_curve():
 
-# def plot_ROC_n_times(clf, X, Y, trps, aucs):
+    tprs = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
 
-#     fig, ax = plt.subplots()
-#     clf.fit(X, y)
-#     viz = metrics.plot_roc_curve(clf, X, Y, alpha=0.3, lw=1, ax=ax)
-#     interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
-#     interp_tpr[0] = 0.0
-#     tprs.append(interp_tpr)
-#     aucs.append(viz.roc_auc)
+    return tprs, aucs, mean_fpr
 
+def plot_ROC_curve_n_times(ax, clf, X_test, y_test, tprs, aucs, mean_fpr, **kwargs):
 
-#     return ax, trps, aucs
+    viz = metrics.plot_roc_curve(clf, X_test, y_test, ax=ax,
+                            	 alpha=0.15, color='b', lw=1, label=None, **kwargs)
+
+    interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
+    interp_tpr[0] = 0.0
+    tprs.append(interp_tpr)
+    aucs.append(viz.roc_auc)
+
+    return tprs, aucs
+
+def plot_ROC_curve_n_mean(ax, tprs, aucs, mean_fpr, **kwargs):
+
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = metrics.auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    ax.plot(mean_fpr, mean_tpr, color='r',
+            label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+            lw=2, alpha=.8, **kwargs)
+
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2, label=None, **kwargs)
+
+    ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05], **kwargs)
+
+    ax.legend(loc="lower right")
+
+    return
