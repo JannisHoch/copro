ee9031e139618e3d758feb5b4cc861ff65bbbac1
Jannis <j.m.hoch@uu.nl>
2020-09-18 12:57:35 +0200

    v0.0.4

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/JannisHoch/conflict_model',
-    version='0.0.4b2',
+    version='0.0.4',
     zip_safe=False,
 )
