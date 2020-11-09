212b637198b9bf296a581285455f7bd1a7047146
Jannis <j.m.hoch@uu.nl>
2020-11-09 13:53:25 +0100

    mini changes

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -1,1 +1,1 @@
-python runner.py ../example/example_settings.cfg -proj ../example/example_settings_proj.cfg -v
+python runner.py ../example/example_settings.cfg -proj ../example/example_settings_proj.cfg
