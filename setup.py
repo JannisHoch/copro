ebf3eee5be574a3836d988dce0dcfa3c16f96d6d
Jannis <j.m.hoch@uu.nl>
2020-11-17 21:21:30 +0100

    towards version 0.0.6b-joss

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/JannisHoch/copro',
-    version='0.0.6b',
+    version='0.0.6b-joss',
     zip_safe=False,
 )
