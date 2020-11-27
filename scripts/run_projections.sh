c8379dc83e8c072b5ac223a8c8dd240f35e517d6
Jannis <j.m.hoch@uu.nl>
2020-11-27 10:35:13 +0100

    renamed command line script

--- scripts/run_projections.sh
+++ scripts/run_projections.sh
@@ -1,1 +1,1 @@
-python runner.py ../example/example_settings.cfg -proj ../example/example_settings_proj.cfg
+python copro_runner.py ../example/example_settings.cfg -proj ../example/example_settings_proj.cfg
