7023efc57f13846ba5cc27167aa22c16227e204f
JannisHoch <j.m.hoch@uu.nl>
2021-01-20 15:12:56 +0100

    updated selection.py

--- copro/selection.py
+++ copro/selection.py
@@ -15,21 +15,26 @@ def filter_conflict_properties(gdf, config):
         geo-dataframe: geo-dataframe containing filtered entries.
     """    
     
+    # create dictinoary with all selection criteria
     selection_criteria = {'best': config.getint('conflict', 'min_nr_casualties'),
                           'type_of_violence': (config.get('conflict', 'type_of_violence')).rsplit(',')}
     
-    print('INFO: filtering on conflict properties.')
+    print('INFO: filtering based on conflict properties.')
     
+    # go through all criteria
     for key in selection_criteria:
 
+        # if nothing is specified, pass
         if selection_criteria[key] == '':
             if config.getboolean('general', 'verbose'): print('DEBUG: passing key', key, 'as it is empty')
             pass
 
+        # for criterion 'best' (i.e. best estimate of fatalities), select all entries above threshold
         elif key == 'best':
             if config.getboolean('general', 'verbose'): print('DEBUG: filtering key', key, 'with lower value', selection_criteria[key])
             gdf = gdf.loc[(gdf[key] >= selection_criteria[key])]
 
+        # for other criteria, select all entries matching the specified value(s) per criterion
         else:
             if config.getboolean('general', 'verbose'): print('DEBUG: filtering key', key, 'with value(s)', selection_criteria[key])
             gdf = gdf.loc[(gdf[key].isin(selection_criteria[key]))]
@@ -47,11 +52,12 @@ def select_period(gdf, config):
         geo-dataframe: geo-dataframe containing filtered entries.
     """    
 
+    # get start and end year of model period
     t0 = config.getint('settings', 'y_start')
     t1 = config.getint('settings', 'y_end')
     
+    # select those entries meeting the requirements
     if config.getboolean('general', 'verbose'): print('DEBUG: focussing on period between {} and {}'.format(t0, t1))
-    
     gdf = gdf.loc[(gdf.year >= t0) & (gdf.year <= t1)]
     
     return gdf
@@ -69,21 +75,27 @@ def clip_to_extent(gdf, config, root_dir):
         geo-dataframe: geo-dataframe containing country polygons of selected continent.
     """    
 
+    # get path to file with polygons for which analysis is carried out
     shp_fo = os.path.join(root_dir, config.get('general', 'input_dir'), config.get('extent', 'shp'))
     
-    print('INFO: reading extent and spatial aggregation level from file {}'.format(shp_fo))
+    # read file
+    if config.getboolean('general', 'verbose'): print('DEBUG: reading extent and spatial aggregation level from file {}'.format(shp_fo))
     extent_gdf = gpd.read_file(shp_fo)
 
-    print('INFO: fixing invalid geometries')
+    # fixing invalid geometries
+    if config.getboolean('general', 'verbose'): print('DEBUG: fixing invalid geometries')
     extent_gdf.geometry = extent_gdf.buffer(0)
 
-    print('INFO: clipping clipping conflict dataset to extent')    
+    # clip the conflict dataframe to the specified polygons
+    if config.getboolean('general', 'verbose'): print('DEBUG: clipping clipping conflict dataset to extent')    
     gdf = gpd.clip(gdf, extent_gdf)
     
     return gdf, extent_gdf
 
 def climate_zoning(gdf, extent_gdf, config, root_dir):
     """This function allows for selecting only those conflicts and polygons falling in specified climate zones.
+    Also, a global dataframe is returned containing the IDs and geometry of all polygons after selection procedure.
+    This can be used to add geometry information to model output based on common ID.
 
     Args:
         gdf (geo-dataframe): geo-dataframe containing conflict data.
@@ -91,57 +103,58 @@ def climate_zoning(gdf, extent_gdf, config, root_dir):
         config (ConfigParser-object): object containing the parsed configuration-settings of the model.
         root_dir (str): path to location of cfg-file.
 
-    Raises:
-        ValueError: raised if a climate zone is specified which is not found in Koeppen-Geiger classification.
-
     Returns:
         geo-dataframe: conflict data clipped to climate zones.
         geo-dataframe: polygons of study area clipped to climate zones.
         dataframe: global look-up dataframe linking polygon ID with geometry information.
     """
 
+    # load file with extents of climate zones
     Koeppen_Geiger_fo = os.path.join(root_dir, config.get('general', 'input_dir'), config.get('climate', 'shp'))
-
-    code2class_fo = os.path.join(root_dir, config.get('general', 'input_dir'), config.get('climate', 'code2class'))
-    
     KG_gdf = gpd.read_file(Koeppen_Geiger_fo)
+    # load file to look-up climate zone names with codes in shp-file
+    code2class_fo = os.path.join(root_dir, config.get('general', 'input_dir'), config.get('climate', 'code2class'))
     code2class = pd.read_csv(code2class_fo, sep='\t')
     
-    if config.get('climate', 'zones') != 'None':
+    # if climate zones are specified...
+    if config.get('climate', 'zones') != '':
 
+        # get all classes specified
         look_up_classes = config.get('climate', 'zones').rsplit(',')
 
+        # get the corresponding code per class
         code_nrs = []
         for entry in look_up_classes:
             code_nr = int(code2class['code'].loc[code2class['class'] == entry])
             code_nrs.append(code_nr)
     
+        # get only those entries with retrieved codes
         KG_gdf = KG_gdf.loc[KG_gdf['GRIDCODE'].isin(code_nrs)]
         
+        # make sure EPSG:4236 is used
         if KG_gdf.crs != 'EPSG:4326':
             KG_gdf = KG_gdf.to_crs('EPSG:4326')
 
+        # clip the conflict dataframe to the specified climate zones
         if config.getboolean('general', 'verbose'): print('DEBUG: clipping conflicts to climate zones {}'.format(look_up_classes))
         gdf = gpd.clip(gdf, KG_gdf.buffer(0))
 
+        # clip the studied polygons to the specified climate zones
         if config.getboolean('general', 'verbose'): print('DEBUG: clipping polygons to climate zones {}'.format(look_up_classes))
         polygon_gdf = gpd.clip(extent_gdf, KG_gdf.buffer(0))
 
-    elif config.get('climate', 'zones') == 'None':
-
-        gdf = gdf.copy()
-        polygon_gdf = extent_gdf.copy()
-
+    # if not, nothing needs to be done besides aligning variable names
     else:
 
-        raise ValueError('no supported climate zone specified - either specify abbreviations of Koeppen-Geiger zones for selection or None for no selection')
+        polygon_gdf = extent_gdf.copy()
 
+    # get a dataframe containing the ID and geometry of all polygons after selecting for climate zones
     global_df = utils.global_ID_geom_info(polygon_gdf)
 
     return gdf, polygon_gdf, global_df
 
 def select(config, out_dir, root_dir):
-    """Main function performing the selection steps.
+    """Main function performing the selection procedure.
     Also stores the selected conflicts and polygons to output directory.
 
     Args:
@@ -156,16 +169,22 @@ def select(config, out_dir, root_dir):
         dataframe: global look-up dataframe linking polygon ID with geometry information.
     """  
 
+    # get the conflict data
     gdf = utils.get_geodataframe(config, root_dir)
 
+    # filter based on conflict properties
     gdf = filter_conflict_properties(gdf, config)
 
+    # selected conflicts falling in a specified time period
     gdf = select_period(gdf, config)
 
+    # clip conflicts to a spatial extent defined as polygons
     gdf, extent_gdf = clip_to_extent(gdf, config, root_dir)
 
+    # clip conflicts and polygons to specified climate zones
     gdf, polygon_gdf, global_df = climate_zoning(gdf, extent_gdf, config, root_dir)
 
+    # save conflict data and polygon to shp-file
     # TODO: save as csv rather than shp
     gdf.to_file(os.path.join(out_dir, 'selected_conflicts.shp'), crs='EPSG:4326')
     polygon_gdf.to_file(os.path.join(out_dir, 'selected_polygons.shp'), crs='EPSG:4326')
