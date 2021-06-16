ec1312fb1977048ba32745b7245853c7eb72f066
JannisHoch <j.m.hoch@uu.nl>
2021-06-16 11:05:14 +0200

    forgot to add content

--- copro/scripts/postprocessing/plot_polygon_vals.py
+++ copro/scripts/postprocessing/plot_polygon_vals.py
@@ -5,13 +5,13 @@ import geopandas as gpd
 import os
 
 @click.command()
-@click.argument('-f', '--file-object',help='path to geojson file_object')
+@click.argument('file-object', type=click.Path())
 @click.option('-c', '--column', help='column name')
 @click.option('-t', '--title', help='title for plot and file_object name')
 @click.option('-v0', '--minimum-value', default=0, type=float)
 @click.option('-v1', '--maximum-value', default=1, type=float)
 @click.option('-cmap', '--color-map', default='brg', type=str)
-@click.argument('-o', '--output-dir', help='path to output directory', type=click.Path())
+@click.argument('output-dir', type=click.Path())
 
 def main(file_object=None, column=None, title=None, minimum_value=None, maximum_value=None, color_map=None, output_dir=None):
     """Quick and dirty function to plot the column values of a geojson file with minimum user input, and save plot.
