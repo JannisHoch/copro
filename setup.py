836ec9cc5d64618552f60bde253a005c6ea9725a
JannisHoch <j.m.hoch@uu.nl>
2021-06-17 09:21:07 +0200

    updated to 0.1.0 including pip support

--- setup.py
+++ setup.py
@@ -4,16 +4,21 @@
 
 from setuptools import setup, find_packages
 
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
 requirements = ['Click>=7.0', 
                 'xarray==0.15.1',
                 'pandas==1.0.3',
+                'GDAL==3.0.4',
                 'rasterio>=1.1.0',
                 'rioxarray>=0.0.26',
                 'rasterstats>=0.14',
                 'geopandas>=0.8.0',
                 'numpy==1.18.1',
                 'scikit-learn>=0.22.1',
-                'seaborn>=0.11']
+                'seaborn>=0.11',
+                'numpy==1.18.1']
 
 setup_requirements = ['pytest-runner', ]
 
@@ -26,14 +31,14 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    description="modelling interplay between conflict and climate",
+    description="Python-model build on scikit-learn functions, designed to facilitate the set-up, execution, and evaluation of machine-learning models for the study of the climate-conflict nexus.",
     entry_points={
         'console_scripts': [
             'copro_runner=copro.scripts.copro_runner:cli',
@@ -41,15 +46,15 @@ setup(
     },
     install_requires=requirements,
     license="MIT",
-    long_description='Python-model build on scikit-learn functions, designed to facilitate the set-up, execution, and evaluation of machine-learning models for the study of the climate-conflict nexus',
+    long_description=readme,
     include_package_data=True,
     keywords='conflict, climate, machine learning, projections',
     name='copro',
-    packages=find_packages(exclude=['docs', 'tests', 'joss']),
+    packages=find_packages(include=['copro', 'copro.*'], exclude=['docs', 'tests', 'joss']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/JannisHoch/copro',
-    version='0.0.8',
+    url='https://copro.readthedocs.io/',
+    version='0.1.0',
     zip_safe=False,
 )
